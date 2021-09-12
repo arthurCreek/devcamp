1794fb84b0e5bcc79a5816f0eacbab2799b73487
Arturo Arroyo <turo310@gmail.com>
2021-09-12 16:55:54 -0700

    Add photo upload for bootcamp

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const colors = require('colors');
+const fileupload = require('express-fileupload');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -21,9 +23,15 @@ const app = express();
 app.use(express.json());
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -31,12 +39,13 @@ app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
-
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
