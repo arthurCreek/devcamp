be27cab166d35e4d76585f2d68a92d26dc29a558
Arturo Arroyo <turo310@gmail.com>
2021-09-12 11:28:58 -0700

    Add mongoose virtuals for cascade delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,7 +28,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // Fingind resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select Fields
   if (req.query.select) {
@@ -132,13 +132,16 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // findByIdAndDelete will not trigger the cascade middleware but this will
+  bootcamp.remove();
 
   res.status(200).json({ success: true, data: {} });
 });
