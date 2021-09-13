d96dab5f687e3354fcb40cb2879e7bc2015ef00b
Arturo Arroyo <turo310@gmail.com>
2021-09-12 18:53:40 -0700

    Send JWT in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,8 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create token
-  const token = user.getSignedJwtTokens();
+  sentTokenResponse(user, 200, res);
 
   res.status(200).json({ success: true, token });
 });
@@ -36,19 +35,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   // Check for user
   const user = await User.findOne({ email }).select('+password');
 
-  if(!user) {
+  if (!user) {
     return next(new ErrorResponse(`Invalid credentials`, 401));
   }
 
   // Check if password matches
   const isMatch = await user.matchPassword(password);
 
-  if(!isMatch) {
+  if (!isMatch) {
     return next(new ErrorResponse(`Invalid credentials`, 401));
   }
 
+  sentTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sentTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtTokens();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    // maxAge: process.env.JWT_COOKIE_EXPIRE,
+    httpOnly: true,
+  };
+
+  if(process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
