aac63f6bf546fb0a2eeb172e346695ec4fae5a70
Arturo Arroyo <turo310@gmail.com>
2021-09-12 18:42:42 -0700

    Add user login route with jwt

--- models/User.js
+++ models/User.js
@@ -48,4 +48,9 @@ UserSchema.methods.getSignedJwtTokens = function () {
   });
 };
 
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model('user', UserSchema);
