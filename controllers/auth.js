6a657daad3440e73af3ea0448f9c95727761931e
Arturo Arroyo <turo310@gmail.com>
2021-09-13 23:25:57 -0700

    Add forgot password route to generate token

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,6 +49,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   sentTokenResponse(user, 200, res);
 });
 
+// @desc    Get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Forgot password
+// @route   GET /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword  = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({email: req.body.email});
+
+  if(!user) {
+    return next(new ErrorResponse(`There is no user with that email`, 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send response
 const sentTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -71,15 +104,3 @@ const sentTokenResponse = (user, statusCode, res) => {
     token,
   });
 };
-
-// @desc    Get current logged in user
-// @route   GET /api/v1/auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
