ed7eef6c364018cb447aaf4f8e5eb2d287dd8fb6
Arturo Arroyo <turo310@gmail.com>
2021-09-14 23:11:57 -0700

    Add average rating calculator to reviews

--- models/Review.js
+++ models/Review.js
@@ -36,5 +36,44 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({bootcamp: 1, user: 1}, { unique: true });
 
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+    const obj = await this.aggregate([
+      {
+        $match: { bootcamp: bootcampId }
+      },
+      {
+        $group: {
+          _id: '$bootcamp',
+          averageRating: { $avg: '$rating' }
+        }
+      }
+    ]);
+  
+    try {
+   if (obj[0]) {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+          averageRating: obj[0].averageRating.toFixed(1)
+        });
+      } else {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+          averageCost: undefined,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function() {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function() {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
 
 module.exports = mongoose.model('Review', ReviewSchema);
