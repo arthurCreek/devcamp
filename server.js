05137d5c11bb1135c228f74e3ba8977e68e44e05
Arturo Arroyo <turo310@gmail.com>
2021-09-15 00:32:12 -0700

    Add index for api documentation

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.use(fileupload());
 app.use(mongoSanitize());
 
 // Set security headers
-app.use(helmet());
+// app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Prevent XSS attacks
 app.use(xss());
